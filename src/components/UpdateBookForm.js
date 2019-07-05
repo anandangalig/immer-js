810233d6dd3fc531c73495d746bafcfb777055b9
Anand Angalig <anandangalig@gmail.com>
2019-07-05 16:28:05 -0400

    re-wrote UpdateBookForm component as class based

--- src/components/UpdateBookForm.js
+++ src/components/UpdateBookForm.js
@@ -1,51 +1,55 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { updateBook } from '../actions';
 
-const UpdateBookForm = props => {
-  const [updateBookName, setupdateBookName] = useState(props.currentBook.name);
-  const [updateBookAuthor, setupdateBookAuthor] = useState(props.currentBook.author);
-
-  const handleSubmit = (name, author) => {
-    const updateData = {
-      id: props.currentBook.id,
-      name: updateBookName,
-      author: updateBookAuthor,
-    };
-    props.updateBook(updateData);
-    props.setEditing(false);
+class UpdateBookForm extends React.Component {
+  state = {
+    updateBookName: this.props.currentBook.name,
+    updateBookAuthor: this.props.currentBook.author,
   };
 
-  return (
-    <form
-      style={{ marginTop: '10px' }}
-      onSubmit={event => {
-        event.preventDefault();
-        handleSubmit(updateBookName, updateBookAuthor);
-      }}
-    >
-      <label htmlFor="book_name">Book Name</label>
-      <input
-        type="text"
-        id="book_name"
-        value={updateBookName}
-        onChange={event => {
-          setupdateBookName(event.target.value);
+  handleSubmit = () => {
+    const updateData = {
+      id: this.props.currentBook.id,
+      name: this.state.updateBookName,
+      author: this.state.updateBookAuthor,
+    };
+    this.props.updateBook(updateData);
+    this.props.setEditing(false);
+  };
+
+  render() {
+    return (
+      <form
+        style={{ marginTop: '10px' }}
+        onSubmit={event => {
+          event.preventDefault();
+          this.handleSubmit();
         }}
-      />
-      <label htmlFor="book_author">Book Author</label>
-      <input
-        type="text"
-        id="book_author"
-        value={updateBookAuthor}
-        onChange={event => {
-          setupdateBookAuthor(event.target.value);
-        }}
-      />
-      <button type="submit">Save</button>
-    </form>
-  );
-};
+      >
+        <label htmlFor="book_name">Book Name</label>
+        <input
+          type="text"
+          id="book_name"
+          value={this.state.updateBookName}
+          onChange={event => {
+            this.setState({ updateBookName: event.target.value });
+          }}
+        />
+        <label htmlFor="book_author">Book Author</label>
+        <input
+          type="text"
+          id="book_author"
+          value={this.state.updateBookAuthor}
+          onChange={event => {
+            this.setState({ updateBookAuthor: event.target.value });
+          }}
+        />
+        <button type="submit">Save</button>
+      </form>
+    );
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   const targetIndex = state.books.findIndex(book => {
